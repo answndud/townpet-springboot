ba7c3ad783c6019c8c3d46b21ce3e56311cd6558
MJY <jmoon0227@gmail.com>
2026-08-11 12:31:41 +0900

    feat: verify media objects before finalize

--- src/main/java/com/townpet/media/MediaService.java
+++ src/main/java/com/townpet/media/MediaService.java
@@ -11,10 +11,13 @@ import org.springframework.transaction.annotation.Transactional;
 class MediaService {
   private final UploadAssetRepository assets;
   private final PublicationAccess publications;
+  private final ObjectStoragePort storage;
 
-  MediaService(UploadAssetRepository assets, PublicationAccess publications) {
+  MediaService(
+      UploadAssetRepository assets, PublicationAccess publications, ObjectStoragePort storage) {
     this.assets = assets;
     this.publications = publications;
+    this.storage = storage;
   }
 
   @Transactional
@@ -31,9 +34,22 @@ class MediaService {
             now.plus(1, ChronoUnit.HOURS)));
   }
 
+  String uploadUrl(UploadAssetEntity asset) {
+    return storage.createUploadUrl(
+        asset.getObjectKey(), asset.getContentType(), asset.getByteSize(), asset.getExpiresAt());
+  }
+
   @Transactional
   UploadAssetEntity finalizeUpload(UUID ownerMemberId, UUID assetId, String checksumSha256) {
     UploadAssetEntity asset = ownedAsset(ownerMemberId, assetId);
+    StoredObject object =
+        storage.inspect(asset.getObjectKey()).orElseThrow(MediaObjectNotFoundException::new);
+    if (!object.contentType().equals(asset.getContentType())
+        || object.byteSize() != asset.getByteSize()
+        || !object.checksumSha256().equalsIgnoreCase(asset.getChecksumSha256())
+        || !object.checksumSha256().equalsIgnoreCase(checksumSha256)) {
+      throw new MediaObjectMismatchException();
+    }
     asset.finalizeUpload(checksumSha256, Instant.now());
     return assets.saveAndFlush(asset);
   }
@@ -60,6 +76,10 @@ class MediaService {
 final class MediaAssetNotFoundException extends RuntimeException {}
 
 final class MediaAssetStateException extends RuntimeException {}
+
+final class MediaObjectNotFoundException extends RuntimeException {}
+
+final class MediaObjectMismatchException extends RuntimeException {}
 
 final class MediaPublicationNotFoundException extends RuntimeException {}
 
