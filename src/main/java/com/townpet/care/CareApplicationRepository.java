04805ab5fcf1d07853044cadc94e3ce6f7ab6d74
MJY <jmoon0227@gmail.com>
2026-08-11 20:16:40 +0900

    feat: add care application workflow

---
+++ src/main/java/com/townpet/care/CareApplicationRepository.java
@@ -0,0 +1,10 @@
+package com.townpet.care;
+
+import java.util.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+interface CareApplicationRepository extends JpaRepository<CareApplicationEntity, UUID> {
+  List<CareApplicationEntity> findByRequestIdOrderByCreatedAtAscIdAsc(UUID requestId);
+  Optional<CareApplicationEntity> findByRequestIdAndApplicantMemberId(UUID requestId, UUID applicantMemberId);
+  Optional<CareApplicationEntity> findByIdAndRequestId(UUID id, UUID requestId);
+}
