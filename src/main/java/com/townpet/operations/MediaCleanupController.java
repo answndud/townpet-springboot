7b37350b5a107e677cd63ef599e185c66beacf9f
MJY <jmoon0227@gmail.com>
2026-08-12 14:46:08 +0900

    feat: add media cleanup operations

--- src/main/java/com/townpet/operations/MediaCleanupController.java
+++ src/main/java/com/townpet/operations/MediaCleanupController.java
@@ -6,9 +6,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 @RestController
 @RequestMapping("/api/v1/operations/media")
+@PreAuthorize("hasRole('MODERATOR')")
 class MediaCleanupController {
   private final MediaOperations media;
 
